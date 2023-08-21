d67b0c28e5662c8cbc124e6319c0f810dd9027c6
Hsiou-Yuan Liu <hyhliu1989@gmail.com>
2023-08-21 12:45:04 -0400

    Day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
+    // part1();
+    part2();
+}
+
+fn part1() {
     let data = include_str!("../input.txt");
     let mut score = 0u32;
     for l in data.lines() {
@@ -35,6 +40,50 @@ fn main() {
     //         .split(|b| *b == b'\n')
     //         .map(|l| ((l[0] - b'A') as i16, (l[2] - b'X') as i16,))
     //         .map(|(a, b)| 1 + b + 3 * (1 + b - a).rem_euclid(3))
+    //         .sum::<i16>(),
+    // );
+}
+
+fn part2() {
+    let data = include_str!("../input.txt");
+    let mut score = 0u32;
+    for l in data.lines() {
+        let char_array: Vec<char> = l.chars().collect();
+        let oppo = (char_array[0] as i8) - ('A' as i8);
+        let mut mine: i8 = 0;
+        let expected_outcome = char_array[2];
+        match expected_outcome {
+            'X' => {
+                // To lose
+                mine = oppo - 1;
+            },
+            'Y' => {
+                // To draw
+                score += 3;
+                mine = oppo;
+            },
+            'Z' => {
+                // To win
+                score += 6;
+                mine = oppo + 1;
+            },
+            _ => {},
+        }
+        match mine.rem_euclid(3) {
+            0 => score += 1,
+            1 => score += 2,
+            2 => score += 3,
+            _ => {},
+        }
+    }
+    println!("{}", score);
+
+    // println!(
+    //     "{}",
+    //     include_bytes!("../input.txt")
+    //         .split(|b| *b == b'\n')
+    //         .map(|l| ((l[0] - b'A') as i16, (l[2] - b'X') as i16,))
+    //         .map(|(a, b)| 1 + b * 3 + (2 + a + b) % 3)
     //         .sum::<i16>(),
     // );
 }
