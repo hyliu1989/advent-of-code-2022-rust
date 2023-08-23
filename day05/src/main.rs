e0238bd3be97fb4d02565018385716bba213c805
Hsiou-Yuan Liu <hyhliu1989@gmail.com>
2023-08-23 14:17:08 -0400

    Day 5 part 1

---
+++ day05/src/main.rs
@@ -0,0 +1,61 @@
+fn main() {
+    part1();
+    println!("==========");
+    // part2();
+}
+
+fn parse_instruction(line: &str) -> (u32, usize, usize) {
+    let (num_inst_str, loc_inst_str) = line.split_once("from").unwrap();
+
+    let (_, num) = num_inst_str.split_once("move ").unwrap();
+    let (src, des) = loc_inst_str.split_once("to").unwrap();
+
+    (
+        num.trim().parse::<u32>().unwrap(),
+        src.trim().parse::<usize>().unwrap() - 1usize,
+        des.trim().parse::<usize>().unwrap() - 1usize,
+    )
+}
+
+fn parse_stack_state(state_str: &str) -> Vec<Vec<char>> {
+    let mut iter = state_str.rsplit("\n");
+    let first_line = iter.next().unwrap();
+    let n_stacks = ((first_line.chars().count() as f64) / 4.0).ceil() as usize;
+
+    let mut ret = Vec::<Vec<char>>::with_capacity(n_stacks);
+    for _ in 0..n_stacks {
+        ret.push(Vec::<char>::new());
+    }
+
+    for line in iter {
+        for (i, crate_str) in line.chars().collect::<Vec<char>>().chunks(4).enumerate() {
+            if crate_str[0] == '[' {
+                ret[i].push(crate_str[1]);
+            }
+        }
+    }
+    ret
+}
+
+fn part1() {
+    let data = include_str!("../input.txt");
+    let (init_state, instructions) = data.split_once("\n\n").unwrap();
+    let mut stack = parse_stack_state(init_state);
+    for line in instructions.lines() {
+        let (num, src, des) = parse_instruction(line);
+        for _ in 0..num {
+            match stack[src].pop() {
+                Some(crate_char) => stack[des].push(crate_char),
+                None => { panic!("Error state!") },
+            }
+        }
+    }
+    let mut ret = String::with_capacity(stack.len());
+    for i in 0..stack.len() {
+        match stack[i].pop() {
+            Some(top_crate_char) => ret.push(top_crate_char),
+            None => {panic!("Empty stack {}", i)},
+        }
+    }
+    println!("{}", ret);
+}
