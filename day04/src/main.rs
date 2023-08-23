795dfe2726f39b598aa5dad9623474578bb12cc4
Hsiou-Yuan Liu <hyhliu1989@gmail.com>
2023-08-23 13:13:02 -0400

    Update the codes with reference, using split_once and filter.

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -3,28 +3,25 @@ fn main() {
 }
 
 
-fn parse_line_part1(l: &str) -> u32 {
-    let elf_asmnt: Vec<Vec<u32>> = l.split(',')
-        .map(|seg| {
-            seg.split('-')
-                .map(|num_str| {num_str.parse::<u32>().unwrap()})
-                .collect::<Vec<u32>>()
-        })
-        .collect();
+fn parse_line_part1(l: &str) -> bool {
+    let (elf1_asmnt_str, elf2_asmnt_str) = l.split_once(',').unwrap();
+    let (elf1_asmnt, elf2_asmnt) = (
+        // Parse the first elf's assignment
+        elf1_asmnt_str.split('-').map(|num_str| num_str.parse::<u32>().unwrap()).collect::<Vec<u32>>(),
+        // Parse the second elf's assignment
+        elf2_asmnt_str.split('-').map(|num_str| num_str.parse::<u32>().unwrap()).collect::<Vec<u32>>(),
+    );
     let containing= |large: &Vec<u32>, small: &Vec<u32>| {
         large[0] <= small[0] && small[0] <= large[1] && large[0] <= small[1] && small[1] <= large[1]
     };
-    if containing(&elf_asmnt[0], &elf_asmnt[1]) || containing(&elf_asmnt[1], &elf_asmnt[0]) {
-        1u32
-    } else {
-        0u32
-    }
+    containing(&elf1_asmnt, &elf2_asmnt) || containing(&elf2_asmnt, &elf1_asmnt)
 }
 
 fn part1() {
     let data = include_str!("../input.txt");
     let num = data.lines()
         .map(|l| { parse_line_part1(l) })
-        .sum::<u32>();
-    println!("{}", num);
+        .filter(|b| *b)
+        .count();
+    println!("{}", num);  // 466
 }
