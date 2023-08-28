b55b115adcb46781bbe20dbd7da5864ac80b6ad0
Hsiou-Yuan Liu <hyhliu1989@gmail.com>
2023-08-27 22:21:05 -0400

    day 8 part 2.

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,11 +1,14 @@
 extern crate ndarray;
 use ndarray::prelude::*;
 
+
+const NUM_HEIGHTS: usize = 10usize;
+
 fn main() {
     let data = include_bytes!("../input.txt");
     part1(data);
     println!("Hello, world!");
-    // part1(data);
+    part2(data);
 }
 
 fn get_dims(data: &[u8]) -> (usize, usize) {
@@ -82,4 +85,72 @@ fn part1(data: &[u8]) {
     let mut counts = 0;
     visible.for_each(|el| { if *el != 0 {counts += 1} });
     println!("{}", counts);
+}
+
+
+fn part2(data: &[u8]) {
+    let forest_height = build_forest_height(data);
+    let (m, n) = forest_height.dim();
+    let mut score = Array2::<u32>::ones((m, n));
+
+    let mut dp_num_seen = Array2::<u32>::ones((n, NUM_HEIGHTS));
+    score.slice_mut(s![0, ..]).fill(0);
+    for i in 1..m {
+        for j in 0..n {
+            let height = forest_height[[i, j]] as usize;
+            score[[i, j]] *= dp_num_seen[[j, height]];
+            for k in 0..(height+1) {
+                dp_num_seen[[j, k]] = 1;
+            }
+            for k in (height+1)..NUM_HEIGHTS {
+                dp_num_seen[[j, k]] += 1;
+            }
+        }
+    }
+    let mut dp_num_seen = Array2::<u32>::ones((n, NUM_HEIGHTS));
+    score.slice_mut(s![m-1, ..]).fill(0);
+    for i in (0..m-1).rev() {
+        for j in 0..n {
+            let height = forest_height[[i, j]] as usize;
+            score[[i, j]] *= dp_num_seen[[j, height]];
+            for k in 0..(height+1) {
+                dp_num_seen[[j, k]] = 1;
+            }
+            for k in (height+1)..NUM_HEIGHTS {
+                dp_num_seen[[j, k]] += 1;
+            }
+        }
+    }
+    let mut dp_num_seen = Array2::<u32>::ones((m, NUM_HEIGHTS));
+    score.slice_mut(s![.., 0]).fill(0);
+    for j in 1..n {
+        for i in 0..m {
+            let height = forest_height[[i, j]] as usize;
+            score[[i, j]] *= dp_num_seen[[i, height]];
+            for k in 0..(height+1) {
+                dp_num_seen[[i, k]] = 1;
+            }
+            for k in (height+1)..NUM_HEIGHTS {
+                dp_num_seen[[i, k]] += 1;
+            }
+        }
+    }
+    let mut dp_num_seen = Array2::<u32>::ones((m, NUM_HEIGHTS));
+    score.slice_mut(s![.., n-1]).fill(0);
+    for j in (0..n-1).rev() {
+        for i in 0..m {
+            let height = forest_height[[i, j]] as usize;
+            score[[i, j]] *= dp_num_seen[[i, height]];
+            for k in 0..(height+1) {
+                dp_num_seen[[i, k]] = 1;
+            }
+            for k in (height+1)..NUM_HEIGHTS {
+                dp_num_seen[[i, k]] += 1;
+            }
+        }
+    }
+
+    let mut max = 0u32;
+    score.for_each(|&v| { if v > max { max = v; }});
+    println!("{}", max);
 }
