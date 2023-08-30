1b3c6e06e3e2b476e51e1ba5b3186fc09650f810
Hsiou-Yuan Liu <hyhliu1989@gmail.com>
2023-08-30 16:50:36 -0400

    day 11 part 2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -5,6 +5,7 @@ struct Monkey {
     holding: Vec<WorryLevel>,
     insp_op: Box<dyn Fn(WorryLevel) -> WorryLevel>,
     test_op: Box<dyn Fn(WorryLevel) -> usize>,
+    test_divisor: u32,
     count: u32,
 }
 
@@ -29,18 +30,20 @@ fn build_monkeys(data: &str) -> Vec<Monkey>{
                     },
                 }
             };
+            let divisor: u32;
             let test_op: Box<dyn Fn(WorryLevel) -> usize> = {
                 let parse = |line: &str, delim: &str| {
                     line.split_once(delim).unwrap().1.parse::<WorryLevel>().unwrap()
                 };
-                let divisor = parse(lines[3], "divisible by ");
+                let divisor_loc = parse(lines[3], "divisible by ");
+                divisor = divisor_loc as u32;
                 let true_ret = parse(lines[4], "throw to monkey ") as usize;
                 let false_ret = parse(lines[5], "throw to monkey ") as usize;
                 Box::new(
-                    move |worry_level| { if worry_level % divisor == 0 { true_ret } else {false_ret} }
+                    move |worry_level| { if worry_level % divisor_loc == 0 { true_ret } else {false_ret} }
                 )
             };
-            Monkey { holding: holding_gen.collect::<Vec<WorryLevel>>(), insp_op, test_op, count: 0 }
+            Monkey { holding: holding_gen.collect::<Vec<WorryLevel>>(), insp_op, test_op, test_divisor: divisor, count: 0 }
         })
         .collect()
 }
@@ -66,7 +69,35 @@ fn part1(data: &str) {
     println!("{}", counts[counts.len()-1] * counts[counts.len()-2]);
 }
 
+fn part2(data: &str) {
+    let mut monkeys = build_monkeys(data);
+
+    // Use modular number to keep the worry level under control.
+    // This would not affect the division test.
+    let worry_level_reduction_div: u32 = monkeys.iter().map(|m| m.test_divisor).product();
+    let worry_level_reduction_div = worry_level_reduction_div as WorryLevel;
+
+    let mut temp_bags = vec![vec![0; 0]; monkeys.len()];
+    for _ in 0..10000 {
+        for (i, m) in monkeys.iter_mut().enumerate() {
+            m.holding.append(&mut temp_bags[i]);
+            for old in m.holding.drain(..) {
+                let worry_level = (m.insp_op)(old) % worry_level_reduction_div;
+                let dest_monkey = (m.test_op)(worry_level);
+                temp_bags[dest_monkey].push(worry_level);
+                m.count += 1;
+            }
+        }
+    }
+
+    let mut counts: Vec<u32> = monkeys.iter().map(|m| m.count).collect();
+    counts.sort();
+    println!("{}", (counts[counts.len()-1] as u64) * (counts[counts.len()-2] as u64));
+}
+
 fn main() {
     let data = include_str!("../input.txt");
     part1(data);
+    println!("=============");
+    part2(data);
 }
