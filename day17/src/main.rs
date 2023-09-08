bfd7cbe654758848f39c181ec82c9ca3241d7de1
Hsiou-Yuan Liu <hyhliu1989@gmail.com>
2023-09-08 03:14:45 +0200

    day 17 part 2
    using Excel for pattern finding after print out (Line 156) rock id and removed height.

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -8,8 +8,9 @@ struct Rock {
 
 const DEBUG: bool = false;
 const WIDTH: usize = 7;
-const NUM_ROCKS: usize = 2022;  // part 1
-// const NUM_ROCKS: usize = 1000000000000;  // part 2
+// const NUM_ROCKS: usize = 2022;  // part 1
+// const NUM_ROCKS: usize = 1_000_000_000_000;  // part 2
+const NUM_ROCKS: usize = 256+304;  // part 2, after observing a repeating pattern.
 
 fn main() {
     let rock_appearing_sequence = &[
@@ -82,15 +83,16 @@ fn main() {
             (ret, idx_rock-1)
         };
     }
-
+    let mut remove_triggering_threshold = 500;
     let mut canvas: Vec<[bool; WIDTH]> = Vec::new();
+    let mut removed_rows: usize = 0;
     let mut current_block_state: Option<(&Rock, i32, i8)> = None;
+    let mut rock_idx = 0usize;
     for instruction in include_bytes!("../input.txt").into_iter().cycle() {
         let (rock, mut i, mut j);
         if let Some((rr, ii, jj)) = current_block_state {
             (i, j, rock) = (ii, jj, rr);
         } else {
-            let rock_idx;
             (i, j) = (canvas.len() as i32 + 3, 2);
             (rock, rock_idx) = get_next_rock();
             if rock_idx == NUM_ROCKS {
@@ -121,6 +123,7 @@ fn main() {
             i -= 1;
             current_block_state = Some((rock, i, j));
         } else {
+            // Create the rock and extend the canvas if needed.
             for (rel_i, rel_j) in &rock.fill {
                 let row_idx = (i + (*rel_i as i32)) as usize;
                 while canvas.len() <= row_idx {
@@ -128,10 +131,43 @@ fn main() {
                 }
                 canvas[row_idx][(j + rel_j) as usize] = true;
             }
+            // Delete canvas rows.
+            // The criteria to show that a chunk of canvas is no longer needed is that the consecutive 4 rows contains
+            // at least one element in every column.
+            if canvas.len() > remove_triggering_threshold {
+                const NUM_TEST_ROWS: usize = 1;
+                let mut removed = false;
+                for (i_rev_row, row_chunk) in canvas.windows(NUM_TEST_ROWS).rev().enumerate() {
+                    if canvas.len() - i_rev_row - NUM_TEST_ROWS <= 0 {
+                        break;
+                    }
+                    let mut ret = row_chunk[0].clone();
+                    for i in 1..NUM_TEST_ROWS {
+                        for j in 0..7 {
+                            ret[j] |= row_chunk[i][j];
+                        }
+                    }
+                    let blocking = {ret.into_iter().reduce(|acc, b| { acc & b })};
+                    if blocking.unwrap() {
+                        let removable_rows = canvas.len() as i32 - i_rev_row as i32 - NUM_TEST_ROWS as i32;
+                        removed_rows += removable_rows as usize;
+                        canvas.drain(0..(removable_rows as usize));
+                        // Print out the rock id and the removed row height and use Excel to analyze the repeating pattern.
+                        println!("{} {}", rock_idx, canvas.len() + removed_rows);
+                        removed = true;
+                        break;
+                    }
+                }
+                if !removed {
+                    remove_triggering_threshold += 500;
+                } else {
+                    remove_triggering_threshold = 500;
+                }
+            }
             current_block_state = None;
         }
     }
-    println!("{}", canvas.len());
+    println!("{}", canvas.len() + removed_rows);
 }
 
 
