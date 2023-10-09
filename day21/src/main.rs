64e80194b82f6f6f75c99cd97c19e4b480bc2ac3
Hsiou-Yuan Liu <hyhliu1989@gmail.com>
2023-10-09 18:33:51 -0400

    Day 21 part 1

---
+++ day21/src/main.rs
@@ -0,0 +1,65 @@
+use std::collections::HashMap;
+use std::cell::RefCell;
+
+
+enum Monkey {
+    Evaluated(i64),
+    Operation {
+        first_operand: String,
+        operator: String,
+        second_operand: String,
+    },
+    Init,
+}
+
+fn get_or_create_monkey<'b, 'a>(
+    name: &'b str, 
+    monkeys_map: &'a mut HashMap<String, RefCell<Monkey>>,
+) -> &'a RefCell<Monkey> {
+    if monkeys_map.get(name).is_none() {
+        monkeys_map.insert(String::from(name), RefCell::new(Monkey::Init)); 
+    }
+    monkeys_map.get(name).unwrap()
+}
+
+fn eval(monkey: &RefCell<Monkey>, monkeys_map: &HashMap<String, RefCell<Monkey>>) -> i64 {
+    match &*monkey.borrow() {
+        Monkey::Evaluated(val) => { *val },
+        Monkey::Operation { first_operand , operator, second_operand } => {
+            let v1 = eval(monkeys_map.get(first_operand).unwrap(), monkeys_map);
+            let v2 = eval(monkeys_map.get(second_operand).unwrap(), monkeys_map);
+            match operator.as_str() {
+                "+" => {v1 + v2},
+                "-" => {v1 - v2},
+                "*" => {v1 * v2},
+                _ => {v1 / v2},
+            }
+        },
+        Monkey::Init => { unreachable!() },
+    }
+}
+
+
+fn main() {
+    println!("Hello, world!");
+    let mut monkeys_map: HashMap<String, RefCell<Monkey>> = HashMap::new();
+    for l in include_str!("../input.txt").lines() {
+        let (name, formula) = l.split_once(": ").unwrap();
+        let monkey = get_or_create_monkey(name, &mut monkeys_map);
+        match *monkey.borrow() {
+            Monkey::Init => {},
+            _ => { panic!("monkey should be just initialized!"); }
+        }
+        
+        if let Ok(value) = formula.parse::<i64>() {
+            *monkey.borrow_mut() = Monkey::Evaluated(value);
+        } else {
+            let mut iter_terms = formula.split(" ");
+            let first_operand= iter_terms.next().unwrap().to_string();
+            let operator = iter_terms.next().unwrap().to_string();
+            let second_operand = iter_terms.next().unwrap().to_string();
+            *monkey.borrow_mut() = Monkey::Operation { first_operand, operator, second_operand };
+        }
+    }
+    println!("{}", eval(monkeys_map.get("root").unwrap(), &monkeys_map))
+}
