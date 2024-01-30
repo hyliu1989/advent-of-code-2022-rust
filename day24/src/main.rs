8479f050b4cb1a16c1959795835ece88a3458289
Hsiou-Yuan Liu <hyhliu1989@gmail.com>
2024-01-30 00:44:21 -0500

    day 24 part 1

---
+++ day24/src/main.rs
@@ -0,0 +1,94 @@
+extern crate ndarray;
+extern crate num;
+extern crate pathfinding;
+
+use ndarray::Array2;
+use pathfinding::prelude::bfs;
+
+
+struct EvolvingMaze {
+    blizzards: Vec<(usize, usize, usize, usize)>,
+    mazes: Vec<Array2<u32>>,
+    shape: (usize, usize),
+}
+
+fn build_maze(data: &[u8]) -> EvolvingMaze {
+    let m = 
+        data.split(|b| *b == b'\n')
+            .filter(|l| l.len() != 0)
+            .count() - 2;
+    let n = 
+        data.split(|b| *b == b'\n')
+            .next()
+            .unwrap()
+            .len() - 2;
+    let mut blizzards: Vec::<(usize, usize, usize, usize)> = Vec::new();
+    for (i, line) in data.split(|b| *b == b'\n').enumerate() {
+        for (j, c) in line.iter().enumerate() {
+            match c {
+                b'<' => {blizzards.push((i-1, j-1, 0, n-1))},
+                b'>' => {blizzards.push((i-1, j-1, 0, 1))},
+                b'^' => {blizzards.push((i-1, j-1, m-1, 0))},
+                b'v' => {blizzards.push((i-1, j-1, 1, 0))},
+                b'.' | b'#' => {},
+                _ => panic!("Invalid input"),
+            };
+        }
+    }
+    let num_mazes = num::integer::lcm(m, n);
+    let mut mazes = Vec::with_capacity(num_mazes);
+    for idx_evolution in 0..num_mazes {
+        mazes.push(Array2::<u32>::zeros((m, n)));
+        for (i, j, di, dj) in blizzards.iter() {
+            let i = (i + idx_evolution * di) % m;
+            let j = (j + idx_evolution * dj) % n;
+            mazes[idx_evolution][[i, j]] += 1;
+        }
+    }
+    EvolvingMaze {blizzards, mazes, shape: (m, n)}
+}
+
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Pos(isize, usize, isize);
+
+fn main() {
+    println!("Hello, world!");
+    let data = include_bytes!("../input.txt");
+    let mazes = build_maze(data);
+    let successors = |p: &Pos| {
+        let idx_evolution = (p.2 + 1) % mazes.mazes.len() as isize;
+        let current_maze = &mazes.mazes[idx_evolution as usize];
+        let (m, n) = &mazes.shape;
+        let mut successors: Vec<Pos> = Vec::new();
+        if p.0 == -1 {
+            successors.push(Pos(p.0, p.1, idx_evolution));
+            if current_maze[[0, 0]] == 0 {
+                successors.push(Pos(0, 0, idx_evolution));
+            }
+        } else {
+            if current_maze[[p.0 as usize, p.1]] == 0 {
+                successors.push(Pos(p.0, p.1, idx_evolution));
+            }
+            if (p.0 as usize) > 0 && current_maze[[(p.0 - 1) as usize, p.1]] == 0 {
+                successors.push(Pos(p.0 - 1, p.1, idx_evolution));
+            }
+            if (p.0 as usize) < m - 1 && current_maze[[(p.0 + 1) as usize, p.1]] == 0 {
+                successors.push(Pos(p.0 + 1, p.1, idx_evolution));
+            }
+            if p.1 > 0 && current_maze[[p.0 as usize, p.1 - 1]] == 0 {
+                successors.push(Pos(p.0, p.1 - 1, idx_evolution));
+            }
+            if p.1 < n - 1 && current_maze[[p.0 as usize, p.1 + 1]] == 0 {
+                successors.push(Pos(p.0, p.1 + 1, idx_evolution));
+            }
+        }
+        successors
+    };
+    let reached = |p: &Pos| { 
+        ((p.0 as usize) == mazes.shape.0 - 1) && (p.1 == mazes.shape.1 - 1)
+    };
+    let result = bfs(&Pos(-1, 0, 0), successors, reached).unwrap();
+    println!("Result: {:?}", result);
+    println!("Result len: {}", result.len());
+}
